a9988ed9843bdedaef7d003da9e70a19c93bd65a
Hennadii Panasiuk <gennapanasuk@gmail.com>
2026-06-15 14:47:48 +0200

    updated seeder in order to create tasks with periods

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,29 @@ namespace Database\Seeders;
 
 use App\Models\Task;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\CarbonPeriod;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
+        $start = now()->startOfMonth()->subMonthsNoOverflow();
+        $end = now();
+        $period = CarbonPeriod::create($start, '1 day', $end);
+
         User::factory(5)
-            ->has(Task::factory()->count(10))
-            ->create();
+            ->create()
+            ->each(function ($user) use($period) {
+                foreach ($period as $date) {
+                    $date->hour(rand(0, 23))->minute(rand(0, 6) * 10);
+
+                    Task::factory()->create([
+                        'user_id' => $user->id,
+                        'created_at' => $date,
+                        'updated_at' => $date
+                    ]);
+                }
+            });
     }
 }
